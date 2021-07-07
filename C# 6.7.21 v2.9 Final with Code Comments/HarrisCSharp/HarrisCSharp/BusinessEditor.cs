7729ecc007d32bc08a14dd966b77ff5a96241100
JordanA95 <47454658+JordanA95@users.noreply.github.com>
2021-07-07 01:47:59 +0100

    C#GitHub7.7.21

---
+++ C# 6.7.21 v2.9 Final with Code Comments/HarrisCSharp/HarrisCSharp/BusinessEditor.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HarrisCSharp
+{
+    public partial class BusinessEditor : Form
+    {
+        //Initialise DbConn Class.
+        DbConn dbConn = new DbConn();
+
+        public BusinessEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void BusinessEditor_Load(object sender, EventArgs e)
+        {
+            //Upon load, initiate the selectAllBusiness procedure to get all records in the Business table of the database.
+            businessdgv.DataSource = dbConn.selectAllBusiness();
+        }
+
+        private void RefreshBusinessbtn_Click(object sender, EventArgs e)
+        {
+            //When Refresh button presed, initialise selectAllBusiness stored procedure to get all records in the Business table of the database.
+            businessdgv.DataSource = dbConn.selectAllBusiness();
+            updateselectedbusinessbtn.Enabled = true;
+            addnewbusinessbtn.Enabled = true;
+            deleteselectedbusinessbtn.Enabled = true;
+            refreshbusinessbtn.Enabled = true;
+        }
+
+        private void businessdgv_CellClick(object sender, EventArgs e)
+        {
+            //When a record in DataGridView is clicked on, each data field value populates the corresponding textbox.
+            int index = Int32.Parse(businessdgv.SelectedCells[0].Value.ToString());
+            businessfnametxb.Text = businessdgv.SelectedCells[1].Value.ToString();
+            businesslnametxb.Text = businessdgv.SelectedCells[1].Value.ToString();
+            businessphonetxb.Text = businessdgv.SelectedCells[1].Value.ToString();
+            businessemailtxb.Text = businessdgv.SelectedCells[1].Value.ToString();
+            businessaddrline1txb.Text = businessdgv.SelectedCells[1].Value.ToString();
+            businessaddrline2txb.Text = businessdgv.SelectedCells[1].Value.ToString();
+            businesscitytxb.Text = businessdgv.SelectedCells[1].Value.ToString();
+            businesspostcodetxb.Text = businessdgv.SelectedCells[1].Value.ToString();
+        }
+
+        private void businessdgv_SelectionChanged(object sender, EventArgs e)
+        {
+            //When a record in DataGridView is clicked on, each data field value populates the corresponding textbox.
+            int index = Int32.Parse(businessdgv.SelectedCells[0].Value.ToString());
+            businessfnametxb.Text = businessdgv.SelectedCells[1].Value.ToString();
+            businesslnametxb.Text = businessdgv.SelectedCells[2].Value.ToString();
+            businessphonetxb.Text = businessdgv.SelectedCells[3].Value.ToString();
+            businessemailtxb.Text = businessdgv.SelectedCells[4].Value.ToString();
+            businessaddrline1txb.Text = businessdgv.SelectedCells[5].Value.ToString();
+            businessaddrline2txb.Text = businessdgv.SelectedCells[6].Value.ToString();
+            businesscitytxb.Text = businessdgv.SelectedCells[7].Value.ToString();
+            businesspostcodetxb.Text = businessdgv.SelectedCells[8].Value.ToString();
+        }
+
+        private void addbusinessbtn_Click(object sender, EventArgs e)
+        {
+            //When addbusinessbtn button clicked, set the values of BusinessContact class variables to the value of the corresponding textbox. Then run the InsertBusiness stored procedure.
+            BusinessContact businessContact = new BusinessContact();
+            businessContact.ContactFName = businessfnametxb.Text;
+            businessContact.ContactLName = businesslnametxb.Text;
+            businessContact.ContactBusinessTel = businessphonetxb.Text;
+            businessContact.ContactEmail = businessemailtxb.Text;
+            businessContact.ContactAddr1 = businessaddrline1txb.Text;
+            businessContact.ContactAddr2 = businessaddrline2txb.Text;
+            businessContact.ContactCity = businesscitytxb.Text;
+            businessContact.ContactPostcode = businesspostcodetxb.Text;
+            dbConn.InsertBusiness(businessContact);
+
+            //Disable all textboxes and buttons except for delete and add new buttons.
+            businessfnametxb.Enabled = false;
+            businesslnametxb.Enabled = false;
+            businessphonetxb.Enabled = false;
+            businessemailtxb.Enabled = false;
+            businessaddrline1txb.Enabled = false;
+            businessaddrline2txb.Enabled = false;
+            businesscitytxb.Enabled = false;
+            businesspostcodetxb.Enabled = false;
+
+            savenewbusinessbtn.Enabled = false;
+            updateselectedbusinessbtn.Enabled = false;
+            saveselectedbusinessbtn.Enabled = false;
+            deleteselectedbusinessbtn.Enabled = false;
+            addnewbusinessbtn.Enabled = false;
+            savenewbusinessbtn.Enabled = false;
+            refreshbusinessbtn.Enabled = true;
+            cancelbtn.Enabled = false;
+
+            //Empty the value of all textboxes.
+            businessfnametxb.Text = string.Empty;
+            businesslnametxb.Text = string.Empty;
+            businessphonetxb.Text = string.Empty;
+            businessemailtxb.Text = string.Empty;
+            businessaddrline1txb.Text = string.Empty;
+            businessaddrline2txb.Text = string.Empty;
+            businesscitytxb.Text = string.Empty;
+            businesspostcodetxb.Text = string.Empty;
+            businessdgv.DataSource = dbConn.selectAllBusiness();
+            statuslbl.Text = "-";
+        }
+
+        private void updatebusinessbtn_Click(object sender, EventArgs e)
+        {
+            //When updatebusinessbtn button clicked, set the value of the BusinessContact class variables to the values of the corresponding textboxes.
+            //Then run UpdateBusiness stored procedure. Then repopulate the DataGridiew by using the selectAllBusiness stored procedure.
+            BusinessContact businessContact = new BusinessContact();
+            businessContact.ContactFName = businessfnametxb.Text;
+            businessContact.ContactLName = businesslnametxb.Text;
+            businessContact.ContactBusinessTel = businessphonetxb.Text;
+            businessContact.ContactEmail = businessemailtxb.Text;
+            businessContact.ContactAddr1 = businessaddrline1txb.Text;
+            businessContact.ContactAddr2 = businessaddrline2txb.Text;
+            businessContact.ContactCity = businesscitytxb.Text;
+            businessContact.ContactPostcode = businesspostcodetxb.Text;
+            dbConn.UpdateBusiness(businessContact);
+            businessdgv.DataSource = dbConn.selectAllBusiness();
+            int index = Int32.Parse(businessdgv.SelectedCells[0].Value.ToString());
+
+            //Enable all textboxes and relevant buttons.
+            businessfnametxb.Enabled = true;
+            businesslnametxb.Enabled = true;
+            businessphonetxb.Enabled = true;
+            businessemailtxb.Enabled = true;
+            businessaddrline1txb.Enabled = true;
+            businessaddrline2txb.Enabled = true;
+            businesscitytxb.Enabled = true;
+            businesspostcodetxb.Enabled = true;
+
+            updateselectedbusinessbtn.Enabled = false;
+            saveselectedbusinessbtn.Enabled = true;
+            deleteselectedbusinessbtn.Enabled = false;
+            addnewbusinessbtn.Enabled = false;
+            savenewbusinessbtn.Enabled = false;
+            refreshbusinessbtn.Enabled = false;
+            cancelbtn.Enabled = true;
+            statuslbl.Text = "You are updating an existing contact";
+        }
+
+        private void deletebusinessbtn_Click(object sender, EventArgs e)
+        {
+            //Run the DeleteBusiness stored procedure after a confirmation message is displayed and yes is selected.
+            string message = "Are you sure?";
+            string caption = "Are you sure you want to delete the selected contact?";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == DialogResult.Yes)
+            {
+                dbConn.DeleteBusiness(Int32.Parse(businessdgv.SelectedCells[0].Value.ToString()));
+
+                businessdgv.DataSource = dbConn.selectAllBusiness();
+            }
+            statuslbl.Text = "-";
+        }
+
+        private void addnewbusinessbtn_Click(object sender, EventArgs e)
+        {
+            //Clear and enable the textboxes and relevant buttons ready for adding a new contact.
+            businessfnametxb.Enabled = true;
+            businesslnametxb.Enabled = true;
+            businessphonetxb.Enabled = true;
+            businessemailtxb.Enabled = true;
+            businessaddrline1txb.Enabled = true;
+            businessaddrline2txb.Enabled = true;
+            businesscitytxb.Enabled = true;
+            businesspostcodetxb.Enabled = true;
+
+            savenewbusinessbtn.Enabled = true;
+            updateselectedbusinessbtn.Enabled = false;
+            saveselectedbusinessbtn.Enabled = false;
+            deleteselectedbusinessbtn.Enabled = false;
+            addnewbusinessbtn.Enabled = false;
+            savenewbusinessbtn.Enabled = true;
+            refreshbusinessbtn.Enabled = false;
+            cancelbtn.Enabled = true;
+
+            businessfnametxb.Text = string.Empty;
+            businesslnametxb.Text = string.Empty;
+            businessphonetxb.Text = string.Empty;
+            businessemailtxb.Text = string.Empty;
+            businessaddrline1txb.Text = string.Empty;
+            businessaddrline2txb.Text = string.Empty;
+            businesscitytxb.Text = string.Empty;
+            businesspostcodetxb.Text = string.Empty;
+            statuslbl.Text = "You are adding a new contact";
+        }
+
+        private void saveselectedbusinessbtn_Click(object sender, EventArgs e)
+        {
+            //Preare for saving a contact to the database by setting the values of businessContact class to the value of the textboxes. Disable all buttons except for Update and AddNew. Then clear the textboxes.
+            int index = Int32.Parse(businessdgv.SelectedCells[0].Value.ToString());
+            BusinessContact businessContact = new BusinessContact();
+            businessContact.ContactID = index;
+            businessContact.ContactFName = businessfnametxb.Text;
+            businessContact.ContactLName = businesslnametxb.Text;
+            businessContact.ContactBusinessTel = businessphonetxb.Text;
+            businessContact.ContactEmail = businessemailtxb.Text;
+            businessContact.ContactAddr1 = businessaddrline1txb.Text;
+            businessContact.ContactAddr2 = businessaddrline2txb.Text;
+            businessContact.ContactCity = businesscitytxb.Text;
+            businessContact.ContactPostcode = businesspostcodetxb.Text;
+            dbConn.UpdateBusiness(businessContact);
+            businessdgv.DataSource = dbConn.selectAllBusiness();
+            
+            businessfnametxb.Enabled = false;
+            businesslnametxb.Enabled = false;
+            businessphonetxb.Enabled = false;
+            businessemailtxb.Enabled = false;
+            businessaddrline1txb.Enabled = false;
+            businessaddrline2txb.Enabled = false;
+            businesscitytxb.Enabled = false;
+            businesspostcodetxb.Enabled = false;
+
+            savenewbusinessbtn.Enabled = false;
+            updateselectedbusinessbtn.Enabled = false;
+            saveselectedbusinessbtn.Enabled = false;
+            deleteselectedbusinessbtn.Enabled = false;
+            addnewbusinessbtn.Enabled = false;
+            savenewbusinessbtn.Enabled = false;
+            refreshbusinessbtn.Enabled = true;
+            cancelbtn.Enabled = false;
+
+            businessfnametxb.Text = string.Empty;
+            businesslnametxb.Text = string.Empty;
+            businessphonetxb.Text = string.Empty;
+            businessemailtxb.Text = string.Empty;
+            businessaddrline1txb.Text = string.Empty;
+            businessaddrline2txb.Text = string.Empty;
+            businesscitytxb.Text = string.Empty;
+            businesspostcodetxb.Text = string.Empty;
+            statuslbl.Text = "-";
+        }
+
+        private void businessdgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //When DataGridView is clicked, the seleted record values are converted to String.
+            DataGridViewRow row = this.businessdgv.Rows[e.RowIndex];
+
+            businessfnametxb.Text = row.Cells[1].Value.ToString();
+            businesslnametxb.Text = row.Cells[2].Value.ToString();
+            businessemailtxb.Text = row.Cells[3].Value.ToString();
+            businessaddrline1txb.Text = row.Cells[4].Value.ToString();
+            businessaddrline2txb.Text = row.Cells[5].Value.ToString();
+            businesscitytxb.Text = row.Cells[6].Value.ToString();
+            businesspostcodetxb.Text = row.Cells[7].Value.ToString();
+            businessphonetxb.Text = row.Cells[8].Value.ToString();
+        }
+
+        private void cancelbtn_Click(object sender, EventArgs e)
+        {
+            //Cancel the adding or updating of a contact by clearing the value of the textboxes and resetting the buttons enabled.
+            businessfnametxb.Text = string.Empty;
+            businesslnametxb.Text = string.Empty;
+            businessphonetxb.Text = string.Empty;
+            businessemailtxb.Text = string.Empty;
+            businessaddrline1txb.Text = string.Empty;
+            businessaddrline2txb.Text = string.Empty;
+            businesscitytxb.Text = string.Empty;
+            businesspostcodetxb.Text = string.Empty;
+
+            savenewbusinessbtn.Enabled = true;
+            updateselectedbusinessbtn.Enabled = true;
+            saveselectedbusinessbtn.Enabled = false;
+            deleteselectedbusinessbtn.Enabled = true;
+            addnewbusinessbtn.Enabled = true;
+            savenewbusinessbtn.Enabled = false;
+            refreshbusinessbtn.Enabled = true;
+            statuslbl.Text = "-";
+        }
+    }
+}
---
+++ C# 6.7.21 v2.9 Final with Code Comments/HarrisCSharp/HarrisCSharp/PersonalEditor.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HarrisCSharp
+{
+    public partial class PersonalEditor : Form
+    {
+
+        //Initialise DbConn for database connection
+        DbConn dbConn = new DbConn();
+
+        public PersonalEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void PersonalEditor_Load(object sender, EventArgs e)
+        {
+            personaldgv.DataSource = dbConn.selectAllPersonal();
+        }
+
+        private void RefreshPersonalbtn_Click(object sender, EventArgs e)
+        {
+            personaldgv.DataSource = dbConn.selectAllPersonal();
+            updateselectedpersonalbtn.Enabled = true;
+            addnewpersonalbtn.Enabled = true;
+            deleteselectedpersonalbtn.Enabled = true;
+            refreshpersonalbtn.Enabled = true;
+        }
+
+        private void personaldgv_CellClick(object sender, EventArgs e)
+        {
+            int index = Int32.Parse(personaldgv.SelectedCells[0].Value.ToString());
+            personalfnametxb.Text = personaldgv.SelectedCells[1].Value.ToString();
+            personallnametxb.Text = personaldgv.SelectedCells[2].Value.ToString();
+            personalphonetxb.Text = personaldgv.SelectedCells[3].Value.ToString();
+            personalemailtxb.Text = personaldgv.SelectedCells[4].Value.ToString();
+            personaladdrline1txb.Text = personaldgv.SelectedCells[5].Value.ToString();
+            personaladdrline2txb.Text = personaldgv.SelectedCells[6].Value.ToString();
+            personalcitytxb.Text = personaldgv.SelectedCells[7].Value.ToString();
+            personalpostcodetxb.Text = personaldgv.SelectedCells[8].Value.ToString();
+        }
+
+        private void personaldgv_SelectionChanged(object sender, EventArgs e)
+        {
+            int index = Int32.Parse(personaldgv.SelectedCells[0].Value.ToString());
+            personalfnametxb.Text = personaldgv.SelectedCells[1].Value.ToString();
+            personallnametxb.Text = personaldgv.SelectedCells[2].Value.ToString();
+            personalphonetxb.Text = personaldgv.SelectedCells[3].Value.ToString();
+            personalemailtxb.Text = personaldgv.SelectedCells[4].Value.ToString();
+            personaladdrline1txb.Text = personaldgv.SelectedCells[5].Value.ToString();
+            personaladdrline2txb.Text = personaldgv.SelectedCells[6].Value.ToString();
+            personalcitytxb.Text = personaldgv.SelectedCells[7].Value.ToString();
+            personalpostcodetxb.Text = personaldgv.SelectedCells[8].Value.ToString();
+        }
+        // code to tell button to take take text from respective fields and add data to database
+        private void addpersonalbtn_Click(object sender, EventArgs e)
+        {
+            PersonalContact personalContact = new PersonalContact();
+            personalContact.ContactFName = personalfnametxb.Text;
+            personalContact.ContactLName = personallnametxb.Text;
+            personalContact.ContactPersonalTel = personalphonetxb.Text;
+            personalContact.ContactEmail = personalemailtxb.Text;
+            personalContact.ContactAddr1 = personaladdrline1txb.Text;
+            personalContact.ContactAddr2 = personaladdrline2txb.Text;
+            personalContact.ContactCity = personalcitytxb.Text;
+            personalContact.ContactPostcode = personalpostcodetxb.Text;
+            dbConn.InsertPersonal(personalContact);
+
+            // Code to Enable/Disable the textboxes once button has been clicked
+            personalfnametxb.Enabled = false;
+            personallnametxb.Enabled = false;
+            personalphonetxb.Enabled = false;
+            personalemailtxb.Enabled = false;
+            personaladdrline1txb.Enabled = false;
+            personaladdrline2txb.Enabled = false;
+            personalcitytxb.Enabled = false;
+            personalpostcodetxb.Enabled = false;
+
+            //Code to Enable/Disable buttons
+            savenewpersonalbtn.Enabled = false;
+            updateselectedpersonalbtn.Enabled = false;
+            saveselectedpersonaldbtn.Enabled = false;
+            deleteselectedpersonalbtn.Enabled = false;
+            addnewpersonalbtn.Enabled = false;
+            savenewpersonalbtn.Enabled = false;
+            refreshpersonalbtn.Enabled = true;
+            cancelbtn.Enabled = false;
+
+            //Code to Empty the text boxes once the fields have been added
+            personalfnametxb.Text = string.Empty;
+            personallnametxb.Text = string.Empty;
+            personalphonetxb.Text = string.Empty;
+            personalemailtxb.Text = string.Empty;
+            personaladdrline1txb.Text = string.Empty;
+            personaladdrline2txb.Text = string.Empty;
+            personalcitytxb.Text = string.Empty;
+            personalpostcodetxb.Text = string.Empty;
+
+            // Code to auto-refresh table
+            personaldgv.DataSource = dbConn.selectAllPersonal();
+
+            statuslbl.Text = "-";
+        }
+
+        private void updatepersonalbtn_Click(object sender, EventArgs e)
+        {
+            PersonalContact personalContact = new PersonalContact();
+            personalContact.ContactFName = personalfnametxb.Text;
+            personalContact.ContactLName = personallnametxb.Text;
+            personalContact.ContactPersonalTel = personalphonetxb.Text;
+            personalContact.ContactEmail = personalemailtxb.Text;
+            personalContact.ContactAddr1 = personaladdrline1txb.Text;
+            personalContact.ContactAddr2 = personaladdrline2txb.Text;
+            personalContact.ContactCity = personalcitytxb.Text;
+            personalContact.ContactPostcode = personalpostcodetxb.Text;
+            dbConn.UpdatePersonal(personalContact);
+            personaldgv.DataSource = dbConn.selectAllPersonal();
+            int index = Int32.Parse(personaldgv.SelectedCells[0].Value.ToString());
+
+            personalfnametxb.Enabled = true;
+            personallnametxb.Enabled = true;
+            personalphonetxb.Enabled = true;
+            personalemailtxb.Enabled = true;
+            personaladdrline1txb.Enabled = true;
+            personaladdrline2txb.Enabled = true;
+            personalcitytxb.Enabled = true;
+            personalpostcodetxb.Enabled = true;
+
+            updateselectedpersonalbtn.Enabled = false;
+            saveselectedpersonaldbtn.Enabled = true;
+            deleteselectedpersonalbtn.Enabled = false;
+            addnewpersonalbtn.Enabled = false;
+            savenewpersonalbtn.Enabled = false;
+            refreshpersonalbtn.Enabled = false;
+            cancelbtn.Enabled = true;
+            statuslbl.Text = "You are updating an existing contact";
+        }
+
+        private void deletepersonalbtn_Click(object sender, EventArgs e)
+        {
+            string message = "Are you sure?";
+            string caption = "Are you sure you want to delete the selected contact?";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == DialogResult.Yes)
+            {
+                dbConn.DeletePersonal(Int32.Parse(personaldgv.SelectedCells[0].Value.ToString()));
+
+                personaldgv.DataSource = dbConn.selectAllPersonal();
+            }
+            statuslbl.Text = "-";
+        }
+
+        private void addnewpersonalbtn_Click(object sender, EventArgs e)
+        {
+            personalfnametxb.Enabled = true;
+            personallnametxb.Enabled = true;
+            personalphonetxb.Enabled = true;
+            personalemailtxb.Enabled = true;
+            personaladdrline1txb.Enabled = true;
+            personaladdrline2txb.Enabled = true;
+            personalcitytxb.Enabled = true;
+            personalpostcodetxb.Enabled = true;
+
+            savenewpersonalbtn.Enabled = true;
+            updateselectedpersonalbtn.Enabled = false;
+            saveselectedpersonaldbtn.Enabled = false;
+            deleteselectedpersonalbtn.Enabled = false;
+            addnewpersonalbtn.Enabled = false;
+            savenewpersonalbtn.Enabled = true;
+            refreshpersonalbtn.Enabled = false;
+            cancelbtn.Enabled = true;
+
+            personalfnametxb.Text = string.Empty;
+            personallnametxb.Text = string.Empty;
+            personalphonetxb.Text = string.Empty;
+            personalemailtxb.Text = string.Empty;
+            personaladdrline1txb.Text = string.Empty;
+            personaladdrline2txb.Text = string.Empty;
+            personalcitytxb.Text = string.Empty;
+            personalpostcodetxb.Text = string.Empty;
+            statuslbl.Text = "You are adding a new contact";
+        }
+
+        private void saveselectedpersonaldbtn_Click(object sender, EventArgs e)
+        {
+            int index = Int32.Parse(personaldgv.SelectedCells[0].Value.ToString());
+            PersonalContact personalContact = new PersonalContact();
+            personalContact.ContactID = index;
+            personalContact.ContactFName = personalfnametxb.Text;
+            personalContact.ContactLName = personallnametxb.Text;
+            personalContact.ContactPersonalTel = personalphonetxb.Text;
+            personalContact.ContactEmail = personalemailtxb.Text;
+            personalContact.ContactAddr1 = personaladdrline1txb.Text;
+            personalContact.ContactAddr2 = personaladdrline2txb.Text;
+            personalContact.ContactCity = personalcitytxb.Text;
+            personalContact.ContactPostcode = personalpostcodetxb.Text;
+            dbConn.UpdatePersonal(personalContact);
+            personaldgv.DataSource = dbConn.selectAllPersonal();
+
+            personalfnametxb.Enabled = false;
+            personallnametxb.Enabled = false;
+            personalphonetxb.Enabled = false;
+            personalemailtxb.Enabled = false;
+            personaladdrline1txb.Enabled = false;
+            personaladdrline2txb.Enabled = false;
+            personalcitytxb.Enabled = false;
+            personalpostcodetxb.Enabled = false;
+
+            savenewpersonalbtn.Enabled = false;
+            updateselectedpersonalbtn.Enabled = false;
+            saveselectedpersonaldbtn.Enabled = false;
+            deleteselectedpersonalbtn.Enabled = false;
+            addnewpersonalbtn.Enabled = false;
+            savenewpersonalbtn.Enabled = false;
+            refreshpersonalbtn.Enabled = true;
+            cancelbtn.Enabled = false;
+
+            personalfnametxb.Text = string.Empty;
+            personallnametxb.Text = string.Empty;
+            personalphonetxb.Text = string.Empty;
+            personalemailtxb.Text = string.Empty;
+            personaladdrline1txb.Text = string.Empty;
+            personaladdrline2txb.Text = string.Empty;
+            personalcitytxb.Text = string.Empty;
+            personalpostcodetxb.Text = string.Empty;
+            statuslbl.Text = "-";
+        }
+
+        private void personaldgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = this.personaldgv.Rows[e.RowIndex];
+
+            personalfnametxb.Text = row.Cells[1].Value.ToString();
+            personallnametxb.Text = row.Cells[2].Value.ToString();
+            personalemailtxb.Text = row.Cells[3].Value.ToString();
+            personaladdrline1txb.Text = row.Cells[4].Value.ToString();
+            personaladdrline2txb.Text = row.Cells[5].Value.ToString();
+            personalcitytxb.Text = row.Cells[6].Value.ToString();
+            personalpostcodetxb.Text = row.Cells[7].Value.ToString();
+            personalphonetxb.Text = row.Cells[8].Value.ToString();
+        }
+
+        private void cancelbtn_Click(object sender, EventArgs e)
+        {
+            personalfnametxb.Text = string.Empty;
+            personallnametxb.Text = string.Empty;
+            personalphonetxb.Text = string.Empty;
+            personalemailtxb.Text = string.Empty;
+            personaladdrline1txb.Text = string.Empty;
+            personaladdrline2txb.Text = string.Empty;
+            personalcitytxb.Text = string.Empty;
+            personalpostcodetxb.Text = string.Empty;
+
+            savenewpersonalbtn.Enabled = true;
+            updateselectedpersonalbtn.Enabled = true;
+            saveselectedpersonaldbtn.Enabled = false;
+            deleteselectedpersonalbtn.Enabled = true;
+            addnewpersonalbtn.Enabled = true;
+            savenewpersonalbtn.Enabled = false;
+            refreshpersonalbtn.Enabled = true;
+            statuslbl.Text = "-";
+        }
+    }
+}
